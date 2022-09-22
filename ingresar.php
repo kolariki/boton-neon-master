e4b9edf938a87efb0478309468496871491f75c6
kolariki <ivankolariki1990@gmail.com>
2022-09-22 14:31:18 -0300

    first commit

---
+++ ingresar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Botón de Luz de Neón con CSS</title>
+    <link rel="stylesheet" href="estilosingreso.css">
+</head>
+
+<body>
+<h1 class="btn-neon">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+        <span id="span4"></span> PORTOLIO WEB: Ivan Kolarik</h1>
+    <a href="#" class="btn-neon">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+        <span id="span4"></span>
+        VER PROYECTOS
+    </a>
+    <a href="#" class="btn-neon">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+        <span id="span4"></span>
+       CONTACTO
+    </a>
+    <a href="#" class="btn-neon">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+        <span id="span4"></span>
+        ACERCA DE MI
+    </a>
+</body>
+</html>
